acb90955e70b6ab67b38e858f8c2ea1ad151df5b
Nuttapon <emperol2@gmail.com>
2016-04-17 18:45:31 +0700

    update post order

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all.paginate(page: params[:page], per_page: 9)
+    @posts = Post.all.paginate(page: params[:page], per_page: 9).order("created_at DESC")
     #@posts_per_page = Post.paginate(:page => params[:page], :per_page => 5)
   end
 
